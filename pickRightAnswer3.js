a10e6a3af14700d862fdd9f20980b0ba776c5df3
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-04-21 18:08:36 +0700

    Update pickRightAnswer3.js

--- pickRightAnswer3.js
+++ pickRightAnswer3.js
@@ -3,7 +3,8 @@ ans = JSON.parse(localStorage.getItem("ans"));
 var buttons = document.querySelectorAll('input[type="radio"]');
 for(i=0;i<cauHoi.length;i++){
 	for(i2=0; i2<ans.length; i2++){
-		if(cauHoi[i].innerText == ans[i2]){
+		x=cauHoi[i].innerText.replace(/(\r\n|\n|\r)/gm,"");
+		if(x == ans[i2]){
 			for(i3=i*4; i3<i*4+4; i3++){
 				if(buttons[i3].parentNode.nextElementSibling.innerText == ans[i2+1]){
 					buttons[i3].onclick();
