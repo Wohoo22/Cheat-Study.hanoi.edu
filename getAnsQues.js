582c2d61de33b5b4438035c6e732529f6ab0afc9
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-05-10 21:10:46 +0700

    Update getAnsQues.js

--- getAnsQues.js
+++ getAnsQues.js
@@ -5,6 +5,6 @@ for(i=2;i<dapAn.length;i++){
 	var question = cauHoi[i-2].innerText; 
 	var answer =  dapAn[i].parentNode.nextElementSibling.innerText;
 	ans.push(question);
-	ans.push(answer);
+	ans.push(answer); 
 }
 localStorage.setItem("ans",JSON.stringify(ans));
