d113a5017cd99ed6c18c6215b528bcf1e4e5fbf2
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-05-10 21:01:45 +0700

    Update getAnsQues.js

--- getAnsQues.js
+++ getAnsQues.js
@@ -4,8 +4,6 @@ ans = [];
 for(i=2;i<dapAn.length;i++){
 	var question = cauHoi[i-2].innerText; 
 	var answer =  dapAn[i].parentNode.nextElementSibling.innerText;
-	question = question.replace(/(\r\n|\n|\r)/gm,"");
-	answer = answer.replace(/(\r\n|\n|\r)/gm,"");
 	ans.push(question);
 	ans.push(answer);
 }
