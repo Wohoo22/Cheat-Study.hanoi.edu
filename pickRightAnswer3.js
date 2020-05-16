2cdfbe647ecf135480edd5bf861ae44ba15a93a4
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-05-16 12:33:51 +0700

    Update pickRightAnswer3.js

--- pickRightAnswer3.js
+++ pickRightAnswer3.js
@@ -1,6 +1,7 @@
 cauHoi = document.getElementsByClassName("col-11 question-box-title");
 ans = JSON.parse(localStorage.getItem("ans"));
 var buttons = document.querySelectorAll('input[type="radio"]');
+pickCount=0;
 for(i=0;i<cauHoi.length;i++){
 	for(i2=0; i2<ans.length; i2++){
 		if(cauHoi[i].innerText == ans[i2]){
@@ -8,8 +9,10 @@ for(i=0;i<cauHoi.length;i++){
 				if(buttons[i3].parentNode.parentNode.parentNode.lastElementChild.innerText == ans[i2+1]){
 					buttons[i3].onclick();
 					buttons[i3].parentNode.parentNode.parentNode.lastElementChild.style.color="red";
+					pickCount++;
 				}
 			}
 		}
 	}
 }
+alert("Đã chọn "+pickCount+"/"+cauHoi.length+", các câu đúng đã được chuyển thành màu đỏ, bạn có thể chọn lại để điểm thấp hơn.");
