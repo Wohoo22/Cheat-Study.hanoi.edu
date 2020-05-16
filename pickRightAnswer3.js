5ca55dbb4da0dcbb57e98b6c14be81cef7e7bbab
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-05-16 11:32:57 +0700

    Update pickRightAnswer3.js

--- pickRightAnswer3.js
+++ pickRightAnswer3.js
@@ -5,7 +5,7 @@ for(i=0;i<cauHoi.length;i++){
 	for(i2=0; i2<ans.length; i2++){
 		if(cauHoi[i].innerText == ans[i2]){
 			for(i3=i*4; i3<i*4+4; i3++){
-				if(buttons[i3].parentNode.nextElementSibling.innerText == ans[i2+1]){
+				if(buttons[i3].parentNode.parentNode.parentNode.lastElementChild.innerText == ans[i2+1]){
 					buttons[i3].onclick();
 				}
 			}
