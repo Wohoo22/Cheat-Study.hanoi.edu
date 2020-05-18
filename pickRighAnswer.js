27830185b2b571cd5e90bff959f9111a9955dc3d
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-05-18 15:18:28 +0700

    Update pickRighAnswer.js

--- pickRighAnswer.js
+++ pickRighAnswer.js
@@ -2,7 +2,7 @@ var buttons = document.querySelectorAll('input[type="radio"]');
 ans = JSON.parse(localStorage.getItem("ans"));
 for(i=0;i<buttons.length;i++){
    for(i2=0;i2<ans.length; i2++){
-      if(buttons[i].parentNode.nextElementSibling.innerText==ans[i2]){
+      if(buttons[i].parentNode.parentNode.parentNode.lastElementChild.innerText==ans[i2]){
          buttons[i].onclick();
       }
    }
