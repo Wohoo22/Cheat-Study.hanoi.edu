79b0678d86d60780d7f4e5d4b50cc7821e9a04eb
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-07-02 13:21:54 +0700

    Update getAnsQues.js

--- getAnsQues.js
+++ getAnsQues.js
@@ -1,9 +1,9 @@
 ans =[];
 dapAn = document.getElementsByClassName("text-success");
 for(i=2;i<dapAn.length;i+=3){
-    var cauHoi =  dapAn[i-1].parentNode.nextElementSibling.innerText.replace(/(\n| |\$|\t)/gm, "");
-	var answer =  dapAn[i].parentNode.nextElementSibling.innerText.replace(/(\n| |\$|\t)/gm, "");
-    ans.push(cauHoi);
+    	var cauHoi =  dapAn[i-1].parentNode.nextElementSibling.innerText.replace(/(\n| |\$|\t)/gm, "").trim();
+	var answer =  dapAn[i].parentNode.nextElementSibling.innerText.replace(/(\n| |\$|\t)/gm, "").trim();
+    	ans.push(cauHoi);
 	ans.push(answer); 
 }
 localStorage.setItem("ans",JSON.stringify(ans));
