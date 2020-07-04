3d81dec946e247687b488598f5b8dcaba62fe91d
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-07-04 19:59:01 +0700

    Update getAnsQues.js

--- getAnsQues.js
+++ getAnsQues.js
@@ -1,8 +1,8 @@
 ans =[];
 dapAn = document.getElementsByClassName("text-success");
 for(i=2;i<dapAn.length;i+=3){
-    	var cauHoi =  dapAn[i-1].parentNode.nextElementSibling.innerText.replace(/(\n| |\$|\t)/gm, "").trim();
-	var answer =  dapAn[i].parentNode.nextElementSibling.innerText.replace(/(\n| |\$|\t)/gm, "").trim();
+    	var cauHoi =  dapAn[i-1].parentNode.nextElementSibling.innerText.replace(/\s/g, "");
+	var answer =  dapAn[i].parentNode.nextElementSibling.innerText.replace(/\s/g, "");
     	ans.push(cauHoi);
 	ans.push(answer); 
 }
