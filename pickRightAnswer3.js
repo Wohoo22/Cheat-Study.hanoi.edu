f7e46e3d983828a9ccdbbd85f5bdc081225819e3
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-07-04 19:59:31 +0700

    Update pickRightAnswer3.js

--- pickRightAnswer3.js
+++ pickRightAnswer3.js
@@ -4,9 +4,9 @@ var buttons = document.querySelectorAll('input[type="radio"]');
 pickCount=0;
 for(i=0;i<cauHoi.length;i++){
 	for(i2=0; i2<ans.length; i2++){
-		if(cauHoi[i].innerText.replace(/(\n| |\$|\t)/gm, "").trim() == ans[i2]){
+		if(cauHoi[i].innerText.replace(/\s/g, "") == ans[i2]){
 			for(i3=i*4; i3<i*4+4; i3++){
-				if(buttons[i3].parentNode.parentNode.parentNode.lastElementChild.innerText.replace(/(\n| |\$|\t)/gm, "").trim() == ans[i2+1]){
+				if(buttons[i3].parentNode.parentNode.parentNode.lastElementChild.innerText.replace(/\s/g, "") == ans[i2+1]){
 					buttons[i3].click();
 					buttons[i3].parentNode.parentNode.parentNode.lastElementChild.style.color="red";
 					pickCount++;
